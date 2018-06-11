68293d9a5cb934bb534261b771a4f73a09a6cdb7
Boubonic Nugz <nugz@canadaecoin.ca>
2018-06-10 23:27:09 -0700

    cleaned up warnings from screenshot edit and added some pretty to the about dialog

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -49,7 +49,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "Actinium"
-#define QAPP_ORG_DOMAIN "Actinium.org"
+#define QAPP_ORG_DOMAIN "actinium.org"
 #define QAPP_APP_NAME_DEFAULT "Actinium-Qt"
 #define QAPP_APP_NAME_TESTNET "Actinium-Qt-testnet"
 
