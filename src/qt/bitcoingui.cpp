9785004d0cd38faef9f419f9c4d9fdb14e6fe0ce
Boubonic Nugz <nugz@canadaecoin.ca>
2018-06-11 02:28:24 -0700

    cleaned up warnings from splashscreen edit and added some pretty to the about dialog - #2

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -338,7 +338,7 @@ void BitcoinGUI::createActions()
     quitAction->setStatusTip(tr("Quit application"));
     quitAction->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q));
     quitAction->setMenuRole(QAction::QuitRole);
-    aboutAction = new QAction(platformStyle->TextColorIcon(":/icons/aboutDialog"), tr("&About %1").arg(tr(PACKAGE_NAME)), this);
+    aboutAction = new QAction(platformStyle->TextColorIcon(":/icons/about"), tr("&About %1").arg(tr(PACKAGE_NAME)), this);
     aboutAction->setStatusTip(tr("Show information about %1").arg(tr(PACKAGE_NAME)));
     aboutAction->setMenuRole(QAction::AboutRole);
     aboutAction->setEnabled(false);
